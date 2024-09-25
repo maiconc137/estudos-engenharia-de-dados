2a274dcd64830cee1f1892723b99dc77b722e116
maiconcGringo <maicon.costa@gringo.com.vc>
2024-09-25 15:35:24 -0300

    finalizando modulos

--- python/modulos/funcoes.py
+++ python/modulos/funcoes.py
@@ -3,3 +3,9 @@ def somar():
 
 def multi():
     print("Essa função vai multiplicar dois valores")
+
+def find_index(to_fnid, item):
+    for i, valor in enumerate(to_fnid):
+        if valor == item:
+            return i
+    return valor
