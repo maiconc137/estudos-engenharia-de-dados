acea95d1e1c6cb3b65403bffe99d93e8ebfa159e
maiconcGringo <maicon.costa@gringo.com.vc>
2024-10-16 15:41:25 -0300

    criando dag group

--- airflow/airflow/dags/dag_group.py
+++ airflow/airflow/dags/dag_group.py
@@ -1,8 +1,9 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+from airflow.utils.taks_group import TaskGroup
 
-dag = DAG('dag_complexa', description="trigger",
+dag = DAG('dag_group', description="group",
           schedule_interval=None, start_date=datetime(2024, 9, 26),
           catchup=False)
 
@@ -12,11 +13,14 @@ task3 = BashOperator(task_id="tsk3", bash_command="exit 1", dag=dag)
 task4 = BashOperator(task_id="tsk4", bash_command="exit 1", dag=dag)
 task5 = BashOperator(task_id="tsk5", bash_command="exit 1", dag=dag)
 task6 = BashOperator(task_id="tsk6", bash_command="exit 1", dag=dag)
-task7 = BashOperator(task_id="tsk7", bash_command="exit 1", dag=dag)
-task8 = BashOperator(task_id="tsk8", bash_command="exit 1", dag=dag)
-task9 = BashOperator(task_id="tsk9", bash_command="sleep 5", dag=dag, trigger_rule='all_failed')
+
+taskGroup = TaskGroup(task_id="taks_group", dag=dag)
+
+task7 = BashOperator(task_id="tsk7", bash_command="exit 1", dag=dag, task_group=taskGroup)
+task8 = BashOperator(task_id="tsk8", bash_command="exit 1", dag=dag, task_group=taskGroup)
+task9 = BashOperator(task_id="tsk9", bash_command="sleep 5", dag=dag, trigger_rule='all_failed', ask_group=taskGroup)
 
 task1 >> task2
 task3 >> task4
 [task2, task4] >> task5 >> task6
-task6 >> [task7, task8, task9]
+task6 >> taskGroup
